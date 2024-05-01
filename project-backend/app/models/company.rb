3f967909f0fce1002f48c1cc6c905198b16871ee
mrstnj <>
2024-05-01 21:38:38 +0900

    add: 企業検索機能

--- project-backend/app/models/company.rb
+++ project-backend/app/models/company.rb
@@ -1,3 +1,10 @@
 class Company < ApplicationRecord
-  enum status: { '無効': 0, '有効': 1 }
+  enum status: { deactivate: 0, activate: 1 }
+
+  def self.search(companies, params)
+    companies = companies.where("code LIKE ?", "%#{params[:code]}%") if params[:code].present?
+    companies = companies.where("name LIKE ?", "%#{params[:name]}%") if params[:name].present?
+    companies = companies.where(status: params[:status]) if params[:status].present?
+    return companies.order(created_at: "desc")
+  end
 end

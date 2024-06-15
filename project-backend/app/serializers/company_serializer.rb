ce96113c1d74a278e131273d6cd017f92774afde
mrstnj <>
2024-06-15 11:43:30 +0900

    add: serializer導入

--- project-backend/app/serializers/company_serializer.rb
+++ project-backend/app/serializers/company_serializer.rb
@@ -1,4 +1,10 @@
 class CompanySerializer < ActiveModel::Serializer
   attributes :id, :name
+  attribute :code, if: :is_show?
+  attribute :status, if: :is_show?
   has_many :services, key: :services_attributes, serializer: ServiceSerializer
+
+  def is_show?
+    instance_options[:is_show].present?
+  end
 end

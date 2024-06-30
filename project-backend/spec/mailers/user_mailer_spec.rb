0a59d378df9ca4c1f78277f4c7fed1ee6384b673
mrstnj <>
2024-06-30 17:00:40 +0900

    add: メール送信rspec

--- project-backend/spec/mailers/user_mailer_spec.rb
+++ project-backend/spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'メソッド動作テスト' do
+    context 'complete_order' do
+      it 'メールが送信されること' do
+        user_plan = FactoryBot.create(:user_plan)
+        FactoryBot.create(:form_item_answer, user_plan: user_plan, name: "sample form item", value: "sample form value")
+        UserMailer.complete_order(user_plan).deliver_now
+        expect(ActionMailer::Base.deliveries.count).to eq(1)
+      end
+    end
+  end
 end

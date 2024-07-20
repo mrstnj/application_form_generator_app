2cd89ad5961c1c7f832966fd46c2a3efd0c4cc1a
mrstnj <>
2024-07-20 12:11:58 +0900

    update: 画像表示微修正

--- project-frontend/src/features/routes/front/top/ServiceCarousel.tsx
+++ project-frontend/src/features/routes/front/top/ServiceCarousel.tsx
@@ -29,7 +29,7 @@ const ServiceCarousel = ({ services }: Props) => {
           interval: 3000,
           type: 'loop',
           gap: '5rem',
-          perPage: isMobile ? 1 : 3,
+          perPage: isMobile ? 1 : 4,
           perMove: 1,
         }}
       >
